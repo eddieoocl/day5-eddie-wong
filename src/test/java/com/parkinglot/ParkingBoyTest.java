2d5ed24e38ea538e01b2e405523e3c515bb40c2f
wonged6 <eddie.wong@oocl.com>
2024-11-22 15:42:55 +0800

    test: add 5 more tests for parking boy

--- src/test/java/com/parkinglot/ParkingBoyTest.java
+++ src/test/java/com/parkinglot/ParkingBoyTest.java
@@ -2,8 +2,8 @@ package com.parkinglot;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ParkingBoyTest {
     @Test
@@ -23,5 +23,123 @@ public class ParkingBoyTest {
             fail();
             return;
         }
+    }
+
+    @Test
+    public void should_return_car_when_fetch_given_a_ticket_and_parking_boy() {
+        // Given
+        ParkingLot parkingLot = new ParkingLot(10);
+        ParkingBoy parkingBoy = new ParkingBoy(parkingLot);
+        Car car = new Car("A12345");
+        Ticket ticket;
+
+        try {
+            // When
+            ticket = parkingBoy.park(car);
+        } catch (NoAvailablePositionException exception) {
+            fail();
+            return;
+        }
+
+        // When
+        try {
+            Car fetchedCar = parkingBoy.fetch(ticket);
+            assertNotNull(fetchedCar);
+        } catch (UnrecognizedTicketException exception) {
+            fail();
+            return;
+        }
+    }
+
+    @Test
+    public void should_return_corrsponding_cars_when_fetch_given_two_cars_and_parking_boy() {
+        // Given
+        ParkingLot parkingLot = new ParkingLot(10);
+        ParkingBoy parkingBoy = new ParkingBoy(parkingLot);
+        Car car1 = new Car("A12345");
+        Car car2 = new Car("B12345");
+        Ticket ticket1;
+        Ticket ticket2;
+        try {
+            ticket1 = parkingBoy.park(car1);
+            ticket2 = parkingBoy.park(car2);
+        } catch (NoAvailablePositionException exception) {
+            fail();
+            return;
+        }
+
+        // When
+        // Then
+        try {
+            Car fetchedCar1 = parkingBoy.fetch(ticket1);
+            Car fetchedCar2 = parkingBoy.fetch(ticket2);
+            assertEquals(car1, fetchedCar1);
+            assertEquals(car2, fetchedCar2);
+        } catch (UnrecognizedTicketException exception) {
+            fail();
+        }
+    }
+
+    @Test
+    public void should_throw_error_message_when_fetch_given_unrecognized_ticket_and_parking_boy() {
+        // Given
+        ParkingLot parkingLot = new ParkingLot(1);
+        ParkingBoy parkingBoy = new ParkingBoy(parkingLot);
+        Ticket ticket = new Ticket(new Car("C123456"));
+
+        // When
+        UnrecognizedTicketException exception = assertThrows(UnrecognizedTicketException.class, () -> {
+            parkingBoy.fetch(ticket);
+        });
+
+        // Then
+        assertEquals(exception.getMessage(), "Unrecognized parking ticket.");
+    }
+
+    @Test
+    public void should_throw_error_message_when_fetch_given_used_ticket_and_parking_boy() {
+        // Given
+        ParkingLot parkingLot = new ParkingLot(1);
+        ParkingBoy parkingBoy = new ParkingBoy(parkingLot);
+        Car car = new Car("A12345");
+        Ticket ticket;
+
+        try {
+            ticket = parkingBoy.park(car);
+        } catch (NoAvailablePositionException exception) {
+            fail();
+            return;
+        }
+
+        try {
+            parkingBoy.fetch(ticket);
+        } catch (UnrecognizedTicketException exception) {
+            fail();
+            return;
+        }
+
+        // When
+        UnrecognizedTicketException exception = assertThrows(UnrecognizedTicketException.class, () -> {
+            parkingBoy.fetch(ticket);
+        });
+
+        // Then
+        assertEquals(exception.getMessage(), "Unrecognized parking ticket.");
+    }
+
+    @Test
+    public void should_throw_error_message_when_park_given_no_space_and_parking_boy() {
+        // Given
+        ParkingLot parkingLot = new ParkingLot(0);
+        ParkingBoy parkingBoy = new ParkingBoy(parkingLot);
+        Car car = new Car("A12345");
+
+        // When
+        NoAvailablePositionException exception = assertThrows(NoAvailablePositionException.class, () -> {
+            parkingBoy.park(car);
+        });
+
+        // Then
+        assertEquals(exception.getMessage(), "No available position.");
     }
 }
