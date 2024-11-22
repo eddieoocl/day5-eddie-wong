7ac7029623b0418b37b9e84eb8c29cf6d9005dab
wonged6 <eddie.wong@oocl.com>
2024-11-22 13:40:45 +0800

    test: add multiple cars test case

--- src/test/java/com/parkinglot/ParkingLotTest.java
+++ src/test/java/com/parkinglot/ParkingLotTest.java
@@ -2,6 +2,7 @@ package com.parkinglot;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class ParkingLotTest {
@@ -30,5 +31,23 @@ public class ParkingLotTest {
 
         // Then
         assertNotNull(fetchedCar);
+    }
+
+    @Test
+    public void should_return_corrsponding_cars_when_fetch_given_two_cars() {
+        // Given
+        ParkingLot parkingLot = new ParkingLot(10);
+        Car car1 = new Car("A12345");
+        Ticket ticket1 = parkingLot.park(car1);
+        Car car2 = new Car("B12345");
+        Ticket ticket2 = parkingLot.park(car2);
+
+        // When
+        Car fetchedCar1 = parkingLot.fetch(ticket1);
+        Car fetchedCar2 = parkingLot.fetch(ticket2);
+
+        // Then
+        assertEquals(car1, fetchedCar1);
+        assertEquals(car2, fetchedCar2);
     }
 }
