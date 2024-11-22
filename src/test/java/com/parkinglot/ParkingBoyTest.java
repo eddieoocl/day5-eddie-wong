d45afccbc0551e932a7b532641356ed40df5aee9
wonged6 <eddie.wong@oocl.com>
2024-11-22 16:51:13 +0800

    test: add 5 more test for story 4

--- src/test/java/com/parkinglot/ParkingBoyTest.java
+++ src/test/java/com/parkinglot/ParkingBoyTest.java
@@ -166,4 +166,138 @@ public class ParkingBoyTest {
             fail();
         }
     }
+
+    @Test
+    public void should_park_second_parking_lot_when_park_given_two_parking_lots_and_parking_lot_one_is_full() {
+        // Given
+        ParkingLot parkingLot1 = new ParkingLot(1);
+        ParkingLot parkingLot2 = new ParkingLot(10);
+        ParkingBoy parkingBoy = new ParkingBoy();
+        parkingBoy.addParkingLot(parkingLot1);
+        parkingBoy.addParkingLot(parkingLot2);
+
+        Car car1 = new Car("A12345");
+        Car car2 = new Car("B12345");
+
+        try {
+            // When
+            parkingBoy.park(car1);
+            Ticket ticket = parkingBoy.park(car2);
+
+            // Then
+            assertNotNull(ticket);
+            assertEquals(parkingLot1.getSize(), 1);
+            assertEquals(parkingLot2.getSize(), 1);
+        } catch (NoAvailablePositionException exception) {
+            fail();
+        }
+    }
+
+    @Test
+    public void should_fetch_corrsponding_ticket_lot_when_fetch_given_two_parking_lots_and_one_car_in_each_parking_lot() {
+        // Given
+        ParkingLot parkingLot1 = new ParkingLot(1);
+        ParkingLot parkingLot2 = new ParkingLot(10);
+        ParkingBoy parkingBoy = new ParkingBoy();
+        parkingBoy.addParkingLot(parkingLot1);
+        parkingBoy.addParkingLot(parkingLot2);
+
+        Car car1 = new Car("A12345");
+        Car car2 = new Car("B12345");
+
+        try {
+            // When
+            Ticket ticket1 = parkingBoy.park(car1);
+            Ticket ticket2 = parkingBoy.park(car2);
+
+            assertEquals(parkingLot1.getSize(), 1);
+            assertEquals(parkingLot2.getSize(), 1);
+
+            Car fetchedCar1 = parkingBoy.fetch(ticket1);
+            Car fetchedCar2 = parkingBoy.fetch(ticket2);
+
+            assertEquals(car1, fetchedCar1);
+            assertEquals(car2, fetchedCar2);
+            assertEquals(parkingLot1.getSize(), 0);
+            assertEquals(parkingLot2.getSize(), 0);
+
+        } catch (Exception exception) {
+            fail();
+        }
+    }
+
+    @Test
+    public void should_throw_error_message_when_fetch_given_unrecognized_ticket_and_two_parking_lots() {
+        // Given
+        ParkingLot parkingLot1 = new ParkingLot(1);
+        ParkingLot parkingLot2 = new ParkingLot(10);
+        ParkingBoy parkingBoy = new ParkingBoy();
+        parkingBoy.addParkingLot(parkingLot1);
+        parkingBoy.addParkingLot(parkingLot2);
+
+        Ticket ticket = new Ticket(new Car("C123456"));
+
+        // When
+        UnrecognizedTicketException exception = assertThrows(UnrecognizedTicketException.class, () -> {
+            parkingBoy.fetch(ticket);
+        });
+
+        // Then
+        assertEquals(exception.getMessage(), "Unrecognized parking ticket.");
+    }
+
+    @Test
+    public void should_throw_error_message_when_fetch_given_used_ticket_and_two_parking_lots() {
+        // Given
+        ParkingLot parkingLot1 = new ParkingLot(1);
+        ParkingLot parkingLot2 = new ParkingLot(10);
+        ParkingBoy parkingBoy = new ParkingBoy();
+        parkingBoy.addParkingLot(parkingLot1);
+        parkingBoy.addParkingLot(parkingLot2);
+
+        Car car = new Car("A12345");
+        Ticket ticket;
+
+        try {
+            ticket = parkingBoy.park(car);
+        } catch (NoAvailablePositionException exception) {
+            fail();
+            return;
+        }
+
+        try {
+            parkingBoy.fetch(ticket);
+        } catch (UnrecognizedTicketException exception) {
+            fail();
+            return;
+        }
+
+        // When
+        UnrecognizedTicketException exception = assertThrows(UnrecognizedTicketException.class, () -> {
+            parkingBoy.fetch(ticket);
+        });
+
+        // Then
+        assertEquals(exception.getMessage(), "Unrecognized parking ticket.");
+    }
+
+    @Test
+    public void should_throw_error_message_when_park_given_no_space_and_two_parking_lots() {
+        // Given
+        ParkingLot parkingLot1 = new ParkingLot(0);
+        ParkingLot parkingLot2 = new ParkingLot(0);
+        ParkingBoy parkingBoy = new ParkingBoy();
+        parkingBoy.addParkingLot(parkingLot1);
+        parkingBoy.addParkingLot(parkingLot2);
+
+        Car car = new Car("A12345");
+
+        // When
+        NoAvailablePositionException exception = assertThrows(NoAvailablePositionException.class, () -> {
+            parkingBoy.park(car);
+        });
+
+        // Then
+        assertEquals(exception.getMessage(), "No available position.");
+    }
 }
