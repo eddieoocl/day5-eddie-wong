f52c31554e21a6696fc4e0a039dd2b351ec262bf
wonged6 <eddie.wong@oocl.com>
2024-11-22 17:37:20 +0800

    test: add one more test for story 5

--- src/test/java/com/parkinglot/SmartParkingBoyTest.java
+++ src/test/java/com/parkinglot/SmartParkingBoyTest.java
@@ -28,4 +28,28 @@ public class SmartParkingBoyTest {
             fail();
         }
     }
+
+    @Test
+    public void should_park_first_parking_lot_when_park_given_two_parking_lots_with_equal_available_capacity() {
+        // Given
+        ParkingLot parkingLot1 = new ParkingLot(10);
+        ParkingLot parkingLot2 = new ParkingLot(10);
+        SmartParkingBoy parkingBoy = new SmartParkingBoy();
+        parkingBoy.addParkingLot(parkingLot1);
+        parkingBoy.addParkingLot(parkingLot2);
+
+        Car car = new Car("A12345");
+
+        try {
+            // When
+            Ticket ticket = parkingBoy.park(car);
+
+            // Then
+            assertNotNull(ticket);
+            assertEquals(parkingLot1.getSize(), 1);
+            assertEquals(parkingLot2.getSize(), 0);
+        } catch (NoAvailablePositionException exception) {
+            fail();
+        }
+    }
 }
